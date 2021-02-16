42da3247fdf41f6cd3f32cd05c75556ef253ca92
Arthur de Barros <69924028+Arthur-de-Barros@users.noreply.github.com>
2021-02-16 15:56:47 -0300

    Initial commit

---
+++ pthontestes/aula06a.py
@@ -0,0 +1,5 @@
+n1 = int(input('Digite um valor: '))
+n2 = int(input('Digite outro número: '))
+soma = n1+n2
+#  print('A soma entre os números', n1, 'e', n2, "é:{}".format(soma))
+print('A soma entre {} e {} vale {}'.format(n1, n2, soma))
