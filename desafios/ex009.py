17cb35a2861a2f31e58cd95dd4d95c334d32f69d
Arthur de Barros <69924028+Arthur-de-Barros@users.noreply.github.com>
2021-02-17 00:26:35 -0300

    Operadores Aritimeticos Exercicios novos e mudados

--- desafios/ex009.py
+++ desafios/ex009.py
@@ -1,7 +1,7 @@
 n = int(input('Digite um Número: '))
 x = 1
 while x < 11:
-    print('{} x {:2} = {}'.format(n, x, (n*x)))
+    print('{} x {:2} = {:2}'.format(n, x, (n*x)))
     x += 1
 else:
     print('Fim tabuada')
---
+++ desafios/ex014.py
@@ -0,0 +1,2 @@
+c = float(input('Digite a temperatura em Celsius: '))
+print('A temperatuda convertida em fahrenheit é: {:.1f}°F'.format((c*9/5)+32))
